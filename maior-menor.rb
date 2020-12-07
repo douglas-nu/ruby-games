14f7820ed54ee690a96e4ab38da9038cb6d49415
Douglas Silva <dogusky.silva@outlook.com>
2020-12-06 22:05:15 -0300

    ajustes no código para o linter

--- maior-menor.rb
+++
@@ -1,8 +0,0 @@
-puts "Bem vindo ao jogo de adivinhação"
-puts "Qual é o seu nome"
-
-nome = gets
-
-5.times { puts }
-
-puts "Vamos iniciar o jogo, " + nome
---
+++ maior_menor.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+puts 'Bem vindo ao jogo de adivinhação'
+puts 'Qual é o seu nome'
+
+nome = gets
+
+5.times { puts }
+
+puts "Vamos iniciar o jogo, #{nome}"
